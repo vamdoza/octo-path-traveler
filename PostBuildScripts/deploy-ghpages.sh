2103344f524bc124a10c91bafd2f0c2cc8ce84a0
vamdoza <vazquezmjj@gmail.com>
2022-02-12 11:24:00 -0800

    Add further commands to debug why dploy to pages is not working

--- PostBuildScripts/deploy-ghpages.sh
+++ PostBuildScripts/deploy-ghpages.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-ls -l
-export buildfolder="$(find . -regex '.\/temp[^\/]*\/default-webgl\/Build' -print -quit)"
+ls
+export buildfolder="$(find . -regex '.\/temp[^\/]*\/default-webgl\/' -print -quit)"
 if [ -z "$buildfolder" ]; then
   echo "Could not find build folder"
   exit 1
@@ -12,12 +12,15 @@ fi
 if [ ! -d ./tmp ]; then
   git clone "https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}" ./tmp
 fi
-cp -r "$buildfolder" ./tmp
-cd ./tmp
-ls
+cp -r "$buildfolder*" ./tmp
 pwd
-echo "$(ls -l)"
-git add Build
+cd ./tmp
+pwd
+ls
+git status
+git add *
 git config --global user.name "$GITHUB_USER"
 git commit -m "unity cloud build"
+git status
+git log -3
 git push --force
