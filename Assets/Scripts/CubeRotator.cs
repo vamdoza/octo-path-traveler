1cbbd7c7fdeece5ea22cd84492b2cd0f312a28a2
vamdoza <vazquezmjj@gmail.com>
2022-02-22 20:54:34 -0800

    Make the cube rotate

---
+++ Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Default
+{
+    public class CubeRotator : MonoBehaviour
+    {
+        public float angularSpeed = 30;
+
+        private void Update()
+        {
+            transform.RotateAround(transform.position, transform.up + transform.right, Time.deltaTime * angularSpeed);
+        }
+    }
+}
